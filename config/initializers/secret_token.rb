76a2400c773a3f355a10259f69fa404e357c8d2e
Brad Henrickson <brad@henrickson.org>
2013-10-04 08:37:26 +0530

    Initial import for wikigps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikigps::Application.config.secret_key_base = '5bc6294522260d018c731857c77dca31c72c623b9de179d20993f8b05eeb19f042c831011625993f8bc45179b9afa1968fa8471613a7679ba2807053a2164821'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wikigps::Application.config.session_store :cookie_store, key: '_wikigps_session'
