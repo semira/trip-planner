d5d163cdfa87d18d70f2c9d5dfa0e4190ea6d66d
Semira Rahemtulla <semira@gmail.com>
2013-12-14 20:43:05 +0700

    removed rjs file that was responding to js request, as all requests are back to being json. updated formatting of dates such that it fully uses moment.js to show day first, then date. all dates get set if the startdate is set upon load of trip. changing the start date and sorting cards keeps the dates in sync on the page and with the server

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Wikigps::Application.initialize!
-
-# date format
-Time::DATE_FORMATS[:day_then_date] = lambda { |time| time.strftime("%a, %-d %b %Y") }
