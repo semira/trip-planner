14aeb95d2706b6fdb13d890b6459a0d1aa8784a1
Semira Rahemtulla <semira@gmail.com>
2014-01-11 22:27:53 +0700

    first day is auto created upon new trip creation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,9 @@
 class Trip < ActiveRecord::Base  
   has_many :days, -> { order("position ASC") }  #lower in list means a higher position number
   
+  after_create :add_first_day
+  
+  def add_first_day
+    self.days.create
+  end
 end
