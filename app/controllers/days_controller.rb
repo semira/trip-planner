46ab82531745f276f055ccae3b90b65d20a8e665
Semira Rahemtulla <semira@gmail.com>
2014-01-12 16:40:13 +0700

    days can now be deleted from trip

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,6 +1,7 @@
 class DaysController < ApplicationController
   include CurrentTrip
-  before_action :set_trip, only: [:create, :update, :update_position]
+  before_action :set_trip #, only: [:create, :update, :update_position]
+  before_action :set_day, only: [:update, :update_position, :destroy]
   
   def create
     @day = @trip.days.create()
@@ -15,7 +16,7 @@ class DaysController < ApplicationController
   end
   
   def update
-    @day = @trip.days.find(params[:pk])
+    #@day = @trip.days.find(params[:pk])
     @day.update_column(params[:name], params[:value]) 
     respond_to do |format|
       if @day.save       
@@ -27,7 +28,7 @@ class DaysController < ApplicationController
   end
   
   def update_position
-    @day = @trip.days.find(params[:pk])
+   # @day = @trip.days.find(params[:pk])
     respond_to do |format|
       if @day.insert_at(params[:newPos].to_i)    
         format.json { render json: @day.as_json }
@@ -37,4 +38,16 @@ class DaysController < ApplicationController
     end
   end
   
+  def destroy
+    @day.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+  
+  private
+
+     def set_day
+       @day = @trip.days.find(params[:pk])
+     end
 end
